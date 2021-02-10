03c557bdd762e51fc5975549174be15363cbd775
redbor <redbor24@gmail.com>
2021-02-10 22:46:09 +0300

    Виноват, был напуган

--- Figure.py
+++ Figure.py
@@ -1,7 +1,14 @@
 import math
 
+class GeomFigure:
+    def get_area(self):
+        return "<abstract method>"
 
-class Rectangle:
+    def __str__(self):
+        return f"{self.__class__.__name__} ()"
+
+
+class Rectangle(GeomFigure):
     def __init__(self, width, length):
         self.width = width
         self.length = length
@@ -9,18 +16,28 @@ class Rectangle:
     def get_area(self):
         return self.length * self.width
 
+    def __str__(self):
+        return f"{self.__class__.__name__} ({self.width}, {self.length})"
 
-class Square(Rectangle):
+
+class Square(GeomFigure):
     def __init__(self, width):
         self.width = width
 
     def get_area(self):
         return self.width ** 2
 
+    def __str__(self):
+        return f"{self.__class__.__name__} ({self.width})"
 
-class Circle:
+
+class Circle(GeomFigure):
     def __init__(self, radius):
         self.radius = radius
 
     def get_area(self):
         return round(2 * math.pi * self.radius ** 2, 4)
+
+    def __str__(self):
+        return f"{self.__class__.__name__} ({self.radius})"
+
